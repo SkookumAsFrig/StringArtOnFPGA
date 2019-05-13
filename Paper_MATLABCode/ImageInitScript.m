804ed93c1fab57c4bc9f1a9b296eff3ea1b375e9
SkookumAsFrig <kss223@cornell.edu>
2019-05-13 19:52:32 -0400

    introduction to website done

--- Paper_MATLABCode/ImageInitScript.m
+++ Paper_MATLABCode/ImageInitScript.m
@@ -1,15 +1,16 @@
 clc
 clear all
 close all
-img = imread('ada.png');
+img = imread('einstein-target.png');
 imgr = imresize(img,[512 512]);
 figure
-imgr = 1*imgr;
+imgr = 1.42*imgr;
 imshow(imgr);
+imwrite(imgr,'einstein_ori.png')
 img2 = 255 - imgr;
 figure
 imshow(img2);
-file = fopen('ada.txt','w');
-img1 = img2(:,:,1);
-fprintf(file,'%x\t',img1);
-fclose(file);
+% file = fopen('ada.txt','w');
+% img1 = img2(:,:,1);
+% fprintf(file,'%x\t',img1);
+% fclose(file);
